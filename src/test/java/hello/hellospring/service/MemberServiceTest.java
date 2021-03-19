8144a43dfc7946069f999922ff51f705355b941d
TAEWAN KIM <twkimd@smilegate.com>
2021-03-19 17:36:42 +0900

    feat : 회원가입 테스트
    - 회원가입 의존성 주입(진행중)

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+
+        //when
+        Long saveId = memberService.join(member);
+
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+
+        //when
+
+        //then
+    }
+
+
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
